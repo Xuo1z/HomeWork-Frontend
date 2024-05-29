b18d3f31514294338196c17a8f63503f7ce72d6a
Xuo1z <149414772+Xuo1z@users.noreply.github.com>
2024-05-29 21:32:30 +0300

    Добавил домен

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:3001/api';
+// export const BASE_URL = 'http://localhost:3001/api';
+export const BASE_URL = 'https://backend-8-xuolz.nomorepartiesco.ru/api';
 
 export const endpoints ={
     games: `${BASE_URL}/games`,
